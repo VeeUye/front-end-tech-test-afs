70971bcc8e74666762b74aee0c939fc8c69e56d8
Whitney Bluzma <whitney@accommodationforstudents.com>
2022-06-01 17:29:45 +0100

    feat: all bills included icon

---
+++ src/components/icons/AllBillsIncludedIcon/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { ReactComponent as AllBillsIncludedIcon } from './all-bills-included-icon.svg'
+
+const Icon = props => <AllBillsIncludedIcon {...props} />
+
+export default Icon

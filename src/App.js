879865b6ed0902fb4bbd7af2f3c9e1cbcf0261f4
Vee Uye <vee.uye@gmail.com>
2022-06-10 10:06:06 +0100

    Pass props to PropertyCard and validate

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Canvas from './components/Canvas'
 import PropertyCard from './components/PropertyCard'
 
 import './App.scss'
+import properties from './data/properties'
 
 function App() {
-  return <Canvas>{<PropertyCard />}</Canvas>
+  return <Canvas>{<PropertyCard properties={properties} />}</Canvas>
 }
 
 export default App
--- src/components/PropertyCard/index.js
+++ src/components/PropertyCard/index.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import '../PropertyCard/styles.scss'
 
-const PropertyCard = () => {
-  return <div>Hello</div>
+const PropertyCard = (properties) => {
+  const {
+    accountType,
+    address,
+    availableFrom,
+    baths,
+    beds,
+    distanceFromUniversity,
+    propertyType,
+    tenancyAgreement,
+    terms,
+    thumbnails,
+  } = properties
 }
 
 export default PropertyCard
+
+PropertyCard.propTypes = {
+  props: PropTypes.arrayOf(
+    PropTypes.shape({
+      accountType: PropTypes.string,
+      address: PropTypes.shape({
+        address2: PropTypes.string,
+      }),
+      availableFrom: PropTypes.string,
+      baths: PropTypes.number,
+      beds: PropTypes.number,
+      distanceFromUniversity: PropTypes.shape({
+        time: PropTypes.string,
+        modeOfTransport: PropTypes.string,
+      }),
+      propertyType: PropTypes.string,
+      tenancyAgreement: PropTypes.string,
+      terms: PropTypes.shape({
+        bills: PropTypes.string,
+        rentPpw: PropTypes.shape({
+          value: PropTypes.number,
+          currency: PropTypes.string,
+        }),
+      }),
+      thumbnails: PropTypes.arrayOf(
+        PropTypes.shape({
+          url: PropTypes.string,
+        })
+      ),
+    })
+  ),
+}
